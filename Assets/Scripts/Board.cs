f553a311b98ec2e942f3a4366202e13a9aaa42de
MrFurlan98 <viniciusrf1998@hotmail.com>
2018-11-09 16:54:01 -0200

    add base game system

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public int height;
+    public int width;
+    public int offSet;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+    public GameObject[] icons;
+    public GameObject[,] allicons;
+    public float score;
+
+    // Use this for initialization
+    void Start () {
+        allTiles = new BackgroundTile[width, height];
+        allicons = new GameObject[width, height];
+        SetUp();
+	}
+
+    private void SetUp()
+    {
+        for(int i=0;i<width;i++)
+        {
+            for(int j=0;j<height;j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j+offSet);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "( " + i + ", " + j + " )";
+                int iconToUse = Random.Range(0, icons.Length);
+
+
+                GameObject icon = Instantiate(icons[iconToUse], tempPosition, Quaternion.identity);
+                icon.GetComponent<Icon>().colunm = j;
+                icon.GetComponent<Icon>().row= i;
+                icon.transform.parent = this.transform;
+                icon.name = "( " + i + ", " + j + " )";
+                allicons[i, j] = icon;
+            }
+        }
+    }
+    public void DestroyMatch()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(allicons[i,j]!=null)
+                {
+                    if (allicons[i, j].GetComponent<Icon>().isMatch)
+                    {
+                        Destroy(allicons[i, j]);
+                        allicons[i, j] = null;
+                    }
+                }
+            }
+        }
+        StartCoroutine(DropColumns());
+    }
+    private IEnumerator DropColumns()
+    {
+        int nullCount = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allicons[i, j] == null)
+                {
+                    nullCount++;
+                }
+                else if(nullCount>0)
+                {
+                    allicons[i, j].GetComponent<Icon>().colunm -= nullCount;
+                    allicons[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoard());
+    }
+    private void RefillBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(allicons[i,j]==null)
+                {
+                    Vector2 tempoPosition = new Vector2(i, j+offSet);
+                    int iconToUse = Random.Range(0, icons.Length);
+                    GameObject newIcon = Instantiate(icons[iconToUse], tempoPosition, Quaternion.identity);
+                    allicons[i, j] = newIcon;
+                    newIcon.GetComponent<Icon>().colunm = j;
+                    newIcon.GetComponent<Icon>().row = i;
+                }
+            }
+        }
+    }
+    private IEnumerator FillBoard()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(.5f);
+    }
+    private void UpdateScore()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Icon.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icon : MonoBehaviour {
+
+    public int colunm;
+    public int row;
+    private Board board;
+    public int targetX;
+    public int targetY;
+    public bool isMatch = false;
+    private Vector2 tempPosition;
+
+
+    // Use this for initialization
+    void Start () {
+        board = FindObjectOfType<Board>();
+        /*targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetX;
+        colunm = targetY;*/
+    }
+    void Update()
+    {
+        if(isMatch)
+        {
+            FindMatch();
+        }
+        targetY = colunm;
+        targetX = row;
+        if(Mathf.Abs(targetY-transform.position.y)>.1)
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            if(board.allicons[row,colunm]!=this.gameObject)
+            {
+                board.allicons[row, colunm] = this.gameObject;
+            }
+        }
+        else
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            
+        }
+    }
+    private void OnMouseDown()
+    {
+        isMatch = true;
+    }
+    private void OnMouseUp()
+    {
+        if (CheckIfCanDestroy())
+        {
+            Scoring();
+            board.DestroyMatch();
+        }
+        else
+        {
+            SetIsMatchFalse();
+
+        }
+    }
+    void FindMatch()
+    {
+        if (targetX > 0)
+        {
+            if(board.allicons[targetX-1,targetY]!=null)
+            {
+                GameObject leftIcon1 = board.allicons[targetX - 1, targetY];
+
+
+                if (leftIcon1.tag == this.gameObject.tag)
+                {
+                    leftIcon1.GetComponent<Icon>().isMatch = true;
+                    //FindMatch(leftIcon1);
+
+                }
+            }
+        }
+        if (targetX < board.width - 1) 
+        {
+            if (board.allicons[targetX + 1, targetY] != null)
+            {
+                GameObject rightIcon1 = board.allicons[targetX + 1, targetY];
+                if (rightIcon1.tag == this.gameObject.tag)
+                {
+                    rightIcon1.GetComponent<Icon>().isMatch = true;
+                    //FindMatch(rightIcon1);
+                }
+            }
+        }
+        if (targetY < board.height - 1) 
+        {
+            if (board.allicons[targetX, targetY + 1] != null)
+            {
+                GameObject upIcon1 = board.allicons[targetX, targetY + 1];
+
+                if (upIcon1.tag == this.gameObject.tag)
+                {
+                    upIcon1.GetComponent<Icon>().isMatch = true;
+                    //FindMatch(upIcon1);
+
+                }
+
+            }
+        }
+        if (targetY > 0)
+        {
+            if (board.allicons[targetX, targetY - 1] != null)
+            {
+                GameObject downIcon1 = board.allicons[targetX, targetY - 1];
+                if (downIcon1.tag == this.gameObject.tag)
+                {
+                    downIcon1.GetComponent<Icon>().isMatch = true;
+                    //FindMatch(downIcon1);
+
+                }
+            }
+        }
+    }
+    public bool CheckIfCanDestroy()
+    {
+        int k = 0;
+        for(int i=0; i <board.width;i++)
+        {
+            for(int j=0; j<board.height;j++)
+            {
+                if(board.allicons[i,j]!=null)
+                {
+                    if (board.allicons[i, j].GetComponent<Icon>().isMatch)
+                    {
+                        k++;
+                    }
+                }
+            }
+        }
+        if(k>=3)
+        {
+            return true;
+        }
+        return false;
+    }
+    private void SetIsMatchFalse()
+    {
+        isMatch = false;
+        if (targetX > 0)
+        {
+            if (board.allicons[targetX - 1, targetY] != null)
+            {
+                board.allicons[targetX - 1, targetY].GetComponent<Icon>().isMatch = false;
+            }
+        }
+        if (targetX < board.width - 1)
+        {
+            if (board.allicons[targetX + 1, targetY] != null)
+            {
+                board.allicons[targetX + 1, targetY].GetComponent<Icon>().isMatch = false;
+            }
+        }
+        if (targetY < board.height - 1)
+        {
+            if (board.allicons[targetX, targetY + 1] != null)
+            {
+                board.allicons[targetX, targetY + 1].GetComponent<Icon>().isMatch = false;
+            }
+        }
+        if (targetY > 0)
+        {
+            if (board.allicons[targetX, targetY - 1] != null)
+            {
+                board.allicons[targetX, targetY - 1].GetComponent<Icon>().isMatch = false;
+            }
+        }
+    }
+    public void Scoring ()
+    {
+
+        float k = 0;
+        for (int i = 0; i < board.width; i++)
+        {
+            for (int j = 0; j < board.height; j++)
+            {
+                if (board.allicons[i, j] != null)
+                {
+                    if (board.allicons[i, j].GetComponent<Icon>().isMatch)
+                    {
+                        k++;
+                    }
+                }
+            }
+        }
+        board.score += k;
+    }
+}
