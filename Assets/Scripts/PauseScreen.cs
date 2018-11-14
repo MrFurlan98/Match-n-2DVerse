270300b04260f2e9d6d5ea3e6757082147f518bc
Esposi <esposivictor@gmail.com>
2018-11-14 12:05:56 -0200

    algumas coisas que estavam faltando no git hehe

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour {
+
+	public string worldMenu;
+
+	private Board board;
+
+	public GameObject thePauseScreen;
+	public GameObject theConfirmationScreen;
+
+	// Use this for initialization
+	void Start () {
+		board = FindObjectOfType<Board> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			thePauseScreen.SetActive (true);
+		}
+	}
+
+	public void Resume()
+	{
+		thePauseScreen.SetActive (false);
+	}
+
+	public void toWorldMenu()
+	{
+		thePauseScreen.SetActive (false);
+		theConfirmationScreen.SetActive (true);
+	}
+
+	public void Sim()
+	{
+		SceneManager.LoadScene (worldMenu);
+	}
+
+	public void Nao()
+	{
+		theConfirmationScreen.SetActive (false);
+	}
+
+	public void Sair()
+	{
+		
+	}
+
+}
