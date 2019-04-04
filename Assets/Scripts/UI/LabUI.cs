04450c0110e83a60bc44505004ce9163f8ae5930
Esposi <esposivictor@gmail.com>
2019-04-04 18:36:57 -0300

    conteudo novo de ui e alterassoins

--- Assets/Scripts/UI/LabUI.cs
+++ Assets/Scripts/UI/LabUI.cs
@@ -8,13 +8,29 @@ public class LabUI : MonoBehaviour {
     [SerializeField]
     private Button m_ExitButton;
 
+    [SerializeField]
+    private Button m_RButton;
+
+    [SerializeField]
+    private Button m_LButton;
+
     private void Start()
     {
         m_ExitButton.onClick = new Button.ButtonClickedEvent();
         m_ExitButton.onClick.AddListener(
-            delegate {
+            delegate
+            {
                 UIManagerBeta.Instance.CloseScreen(UIManagerBeta.BUTTONS.LAB);
             });
+
+        m_RButton.onClick = new Button.ButtonClickedEvent();
+        //m_RButton.onClick.AddListener();
+
+
+
+        m_LButton.onClick = new Button.ButtonClickedEvent();
+        //m_LButton.onClick.AddListener();
+
 
     }
 }
--- Assets/Scripts/UI/StoreUI.cs
+++ Assets/Scripts/UI/StoreUI.cs
@@ -9,13 +9,30 @@ public class StoreUI : MonoBehaviour
     [SerializeField]
     private Button m_ExitButton;
 
+    [SerializeField]
+    private Button m_RButton;
+
+    [SerializeField]
+    private Button m_LButton;
+
     private void Start()
     {
         m_ExitButton.onClick = new Button.ButtonClickedEvent();
         m_ExitButton.onClick.AddListener(
-            delegate {
+            delegate
+            {
                 UIManagerBeta.Instance.CloseScreen(UIManagerBeta.BUTTONS.STORE);
             });
+
+
+        m_RButton.onClick = new Button.ButtonClickedEvent();
+        //m_RButton.onClick.AddListener();
+
+
+
+        m_LButton.onClick = new Button.ButtonClickedEvent();
+        //m_LButton.onClick.AddListener();
+
 
     }
 }
