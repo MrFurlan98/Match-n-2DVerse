302328f93067773ca0f471391b9e65f1e2692172
MrFurlan98 <viniciusrf1998@hotmail.com>
2019-04-05 11:45:43 -0300

    add destroy by block fixed

--- Assets/Scripts/BoardActions/DestroyBlock.cs
+++ Assets/Scripts/BoardActions/DestroyBlock.cs
@@ -15,9 +15,9 @@ public class DestroyBlock : BaseAction {
     {
         Debug.Log("entrou");
         Vector2Int tNxN = new Vector2Int(NxN.x / 2, NxN.y / 2);
-        for (int i = pOriginX-tNxN.x; i < pOriginX + tNxN.x && pIcons.GetLength(0)>= pOriginX + tNxN.x; i++)
+        for (int i = pOriginX-tNxN.x; i <= pOriginX + tNxN.x && pIcons.GetLength(0)>= pOriginX + tNxN.x; i++)
         {
-            for (int j = pOriginY-(NxN.y/2); j < pOriginY + (NxN.y / 2) && pIcons.GetLength(1)>= pOriginY + (NxN.y / 2); j++)
+            for (int j = pOriginY-(NxN.y/2); j <= pOriginY + (NxN.y / 2) && pIcons.GetLength(1)>= pOriginY + (NxN.y / 2); j++)
             {
                 if (pIcons[i, j].StateIcon != BoardIcon.E_State.CANT_DESTROY)
                     pIcons[i, j].StateIcon = BoardIcon.E_State.MARK_TO_DESTROY;
