d440a4bbc790d42ddb3986799f7f0ebb60b3151a
Felipexox <felipeaa100@gmail.com>
2019-06-02 10:19:58 -0300

    [Commit]   - Removed playfab sdk   - Added gamesparks sdk   - Update UI roadmap

--- Assets/Scripts/Backend/Backend.cs
+++ Assets/Scripts/Backend/Backend.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using PlayFab.Events;
-using PlayFab.ClientModels;
-using PlayFab;
+using GameSparks.Core;
 public partial class Backend: MonoBehaviour {
     private static Backend m_Instance;
 
-    private static string GUEST_KEY= "GUEST_ID";
-    private static string GUEST_USERNAME = "GUEST_USERNAME";
 
     private void Awake()
     {
         Instance = this;
+        StartCoroutine(VerifyServerConnection());
+    }
+
+    IEnumerator VerifyServerConnection()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            yield return new WaitForSeconds(2);
+            if (GS.Available)
+            {
+                //UIManagerBeta.Instance.CloseScreen(UIManagerBeta.SCREENS.LOADING);
+                //UIManagerBeta.Instance.OpenScreen(UIManagerBeta.SCREENS.AUTHETICATION);
+                UIManager.Instance.CloseScreen<LoadingUI>();
+                UIManager.Instance.OpenScreen<AuthenticationUI>();
+                break;
+            }
+        }
+    }
+
+    public void DailyLogin(Action<int> pSucessAction)
+    {
+
     }
 
     public static Backend Instance
---
+++ Assets/Scripts/Utility/PersistInformation.cs
@@ -0,0 +1,47 @@
+﻿
+using Newtonsoft.Json;
+using System.IO;
+
+public class PersistInformation
+{
+    public enum INFO_TYPE {
+        PROFILE,
+        AUTHENTICATION,
+        PROGRESS
+    }
+
+    public static void PersistData(INFO_TYPE pType, object pContent)
+    {
+        PersistJSON(pType.ToString(), JsonConvert.SerializeObject( pContent ));
+    }
+
+    public static T LoadData<T>(INFO_TYPE pType) where T : new()
+    {
+        string pContents = LoadJSON(pType.ToString().ToUpper());
+
+        if (!string.IsNullOrEmpty(pContents))
+        {
+            return JsonConvert.DeserializeObject<T>(pContents);
+        }else
+        {
+            return new T();
+        }
+    }
+
+    private static void PersistJSON(string pNameFile, string pContent)
+    {
+        string tPath = UnityEngine.Application.persistentDataPath + "/" + pNameFile + ".json";
+
+        File.WriteAllText(tPath, pContent);
+    }
+
+    private static string LoadJSON(string pNameFile)
+    {
+        string tPath = UnityEngine.Application.persistentDataPath + "/" + pNameFile + ".json";
+
+        if (File.Exists(tPath))
+            return File.ReadAllText(tPath);
+
+        return null;
+    }
+}
